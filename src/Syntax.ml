f8f333cb068432c1a813f841f80ab08c08372177
rosshka <rosshka@gmail.com>
2019-03-31 19:46:24 +0300

    hw2

--- src/Syntax.ml
+++ src/Syntax.ml
@@ -34,6 +34,28 @@ module Expr =
     *)
     let update x v s = fun y -> if x = y then v else s y
 
+let bool_to_int x = if x then 1 else 0
+let int_to_bool x = if x == 0 then false else true
+
+let boolop_with_ret_to_int op = fun lhs rhs -> bool_to_int (op (int_to_bool lhs) (int_to_bool rhs))
+let boolop_to_int op = fun lhs rhs -> bool_to_int (op lhs rhs)
+
+let str_to_op op = match op with
+    | "+" -> ( + )
+    | "-" -> ( - )
+    | "*" -> ( * )
+    | "/" -> ( / )
+    | "%" -> ( mod )
+    | "!!" -> boolop_with_ret_to_int ( || )
+    | "&&" -> boolop_with_ret_to_int ( && )
+    | "==" -> boolop_to_int ( == )
+    | "!=" -> boolop_to_int ( != )
+    | "<=" -> boolop_to_int ( <= )
+    | "<" -> boolop_to_int ( < )
+    | ">=" -> boolop_to_int ( >= )
+    | ">" -> boolop_to_int ( > )
+    | _ -> failwith "unsupported op"
+
     (* Expression evaluator
 
           val eval : state -> t -> int
@@ -41,7 +63,12 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+let rec eval s e = match e with
+    | Const x -> x
+    | Var n -> s n
+    | Binop (op, e1, e2) -> let r1 = eval s e1 in
+                            let r2 = eval s e2 in
+                            str_to_op op r1 r2
 
   end
                     
@@ -65,7 +92,14 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval config statement = 
+      let (state, input, output) = config in
+      match statement with
+        | Read variable_name -> (match input with
+          | head::tail -> (Expr.update variable_name head state, tail, output))
+        | Write expression -> (state, input, output @ [Expr.eval state expression])
+        | Assign (variable_name, expression) -> (Expr.update variable_name (Expr.eval state expression) state, input, output)
+        | Seq (statement1, statement2) -> eval (eval config statement1) statement2;;
                                                          
   end
 
